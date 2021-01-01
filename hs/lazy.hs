4e1568c401e6199b14d5d3235c988b6ede3b01fb
daujerrine <aghorui@teknik.io>
2021-01-01 12:20:14 +0530

    Haskell lazy/strict examples

---
+++ hs/lazy.hs
@@ -0,0 +1,10 @@
+
+avg xs = q / p
+    where
+        g = foldl (\(a,b) c -> (a + 1,b + c)) (0,0) xs
+        p = fst g
+        q = snd g
+
+main :: IO()
+main = do
+    putStrLn $ show $ avg [0 .. 4000000]
